1d7eb276cb351e8bcfaa6c86e66632e7d638cb24
Prathamesh Patwekar <pp7138@rit.edu>
2024-12-05 20:14:44 -0500

    added timing info in controller

--- ProblematicArchitecture/SearchService/src/main/java/com/example/demo/controller/CatalogController.java
+++ ProblematicArchitecture/SearchService/src/main/java/com/example/demo/controller/CatalogController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Catalog;
 import com.example.demo.service.CatalogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +16,22 @@ public class CatalogController {
 
     private CatalogService catalogService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CatalogController.class);
+
     public CatalogController(CatalogService catalogService) {
         this.catalogService = catalogService;
     }
 
     @GetMapping("/")
     public ResponseEntity<Catalog> getCatalog(){
+        long startTime = System.currentTimeMillis();
+
         Catalog catalog = catalogService.getCatalog();
+
+        long endTime = System.currentTimeMillis();
+
+        logger.info("TOTAL TIME REQUIRED="+(endTime-startTime)+"ms");
+
         return new ResponseEntity<>(catalog, HttpStatus.OK);
     }
 }
